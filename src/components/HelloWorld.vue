92930e557840a0eb48763c90520a658b049c7245
Chimannn <1164617068@qq.com>
2023-04-14 17:11:46 +0800

    feat:引入echarts,实现中国地图展示，实现动画切换

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,10 +8,14 @@ onMounted(()=>{
     initDataFunc(data)
 })
 
-const { chinaTotal, gdData } = toRefs(data)
+const { chinaTotal, gdData, mapType } = toRefs(data)
 
 const tabChange = (type: number) => {
     data.type = type
+}
+
+const mapTypeChange = (type: number) => {
+    data.mapType = type
 }
 
 </script>
@@ -106,102 +110,207 @@ const tabChange = (type: number) => {
             </div>
             
         </div>
+
+        <!-- echarts -->
+        <div class="data-map content">
+            <h3>中国疫情</h3>
+            <div class="map-box">
+                <div :class="mapType == 1 ? 'to-left' : 'to-right'" id="map"></div>
+                <div :class="mapType == 1 ? 'to-left' : 'to-right'" id="map2"></div>
+            </div>
+            <div class="map-btn">
+                <div @click="mapTypeChange(1)" :class="{ active: mapType == 1 }">
+                现有确诊
+                </div>
+                <div @click="mapTypeChange(2)" :class="{ active: mapType == 2 }">
+                累计确诊
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    .bg-img{
-        width: 100%;
+@keyframes toRight {
+    from {
+        left: 0;
     }
+    to {
+        left: calc(0px - 100vw + 1rem);
+    }
+    }
+@keyframes toLeft {
+    from {
+        left: calc(0px - 100vw + 1rem);
+    }
+    to {
+        left: 0;
+    }
+}
 
-    .top-box{
-        position: relative;
-        .title-text{
-            position: absolute;
-            color: #fff;
-            top: 20px;
-            left: 20px;
-            font-size: 14px !important;
-            h1,h2{
-                margin: 0;
-            }
+.bg-img{
+    width: 100%;
+}
+
+.top-box{
+    position: relative;
+    .title-text{
+        position: absolute;
+        color: #fff;
+        top: 20px;
+        left: 20px;
+        font-size: 14px !important;
+        h1,h2{
+            margin: 0;
         }
-        .cover-cards{
-            h4,p{
-                margin: 0;
-            }
-            font-size: 15px;
-            position: absolute;
-            top: 140px;
-            background-color: #fff;
-            border-radius: 20px;
-            width: calc( 100% - 2rem );
-            left: 1rem;
-            overflow: hidden;
-            box-shadow: 0 2px 20px rgb(0 0 0 / 10%);
-            > div{
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                flex-wrap: wrap;
-                &.cover-tab{
-                    li{
-                        display: inline-block;
-                        margin-top: 13px;
-                        margin-right: 3px;
-                        width: 11px;
-                        height: 14px;
-                        background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAaCAMAAACAXYxPAAAAgVBMVEUAAAAzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzNi1y5rAAAAKnRSTlMAuQ/daEsK+eTDoUTvycGojVc/Oh4U88+ygHosJQXg1NKscDPqlmJdKRh9lJelAAAA8ElEQVQoz12QiXKDMAxE1wFCIEBDbgq5mp7v/z+wwh5Dp28GeSUseW0FdpsV5E36o4nBAXWRAK+xlkG5k3FoE5w8a9gqsgnVBew1U5JaPPt9uwZOrYwcaU9hKoW6ho3JN1r7bOPAdW+DCjKLvKhhITkOMi6sLJKIo62WeBxLaYWoxuHv8hSE4mlcH/TePElo73iOFnK7Un9jGw56eh8tJLdw80++pAoZfQluLePKZfSd6g/fPGQc7dfMKVjObPLEOiaOTJGKZRDLnIUCH6Rzzz2KQhMdZXhva5k5e9+1Hz5zt7ykUyQcduSM0z+GiibqX4FjFj/TNw/kAAAAAElFTkSuQmCC");
-                        background-size: 100% 100%;
-                    }
-                    > div {
-                        width: 50%;
-                        background-color: #efefef;
-                        text-align: center;
-                        height: 40px;
-                        line-height: 40px;
-                        &.active{
-                            background-color: #fff;
-                        }
+    }
+    .cover-cards{
+        h4,p{
+            margin: 0;
+        }
+        font-size: 15px;
+        position: absolute;
+        top: 140px;
+        background-color: #fff;
+        border-radius: 20px;
+        width: calc( 100% - 2rem );
+        left: 1rem;
+        overflow: hidden;
+        box-shadow: 0 2px 20px rgb(0 0 0 / 10%);
+        > div{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-wrap: wrap;
+            &.cover-tab{
+                li{
+                    display: inline-block;
+                    margin-top: 13px;
+                    margin-right: 3px;
+                    width: 11px;
+                    height: 14px;
+                    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAaCAMAAACAXYxPAAAAgVBMVEUAAAAzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzNi1y5rAAAAKnRSTlMAuQ/daEsK+eTDoUTvycGojVc/Oh4U88+ygHosJQXg1NKscDPqlmJdKRh9lJelAAAA8ElEQVQoz12QiXKDMAxE1wFCIEBDbgq5mp7v/z+wwh5Dp28GeSUseW0FdpsV5E36o4nBAXWRAK+xlkG5k3FoE5w8a9gqsgnVBew1U5JaPPt9uwZOrYwcaU9hKoW6ho3JN1r7bOPAdW+DCjKLvKhhITkOMi6sLJKIo62WeBxLaYWoxuHv8hSE4mlcH/TePElo73iOFnK7Un9jGw56eh8tJLdw80++pAoZfQluLePKZfSd6g/fPGQc7dfMKVjObPLEOiaOTJGKZRDLnIUCH6Rzzz2KQhMdZXhva5k5e9+1Hz5zt7ykUyQcduSM0z+GiibqX4FjFj/TNw/kAAAAAElFTkSuQmCC");
+                    background-size: 100% 100%;
+                }
+                > div {
+                    width: 50%;
+                    background-color: #efefef;
+                    text-align: center;
+                    height: 40px;
+                    line-height: 40px;
+                    &.active{
+                        background-color: #fff;
                     }
                 }
-                &.cover-info{
-                    margin-top: 10px;
-                    font-size: 12px;
-                    flex-direction: column;
-                    > div {
-                        display: flex;
-                        width: 100%;
-                        justify-content: space-between;
-                        margin-bottom: 10px;
-                        > div{
-                            p:nth-child(3){
-                                color: #999;
-                            }
-                            width: 50%;
-                            text-align: center;
+            }
+            &.cover-info{
+                margin-top: 10px;
+                font-size: 12px;
+                flex-direction: column;
+                > div {
+                    display: flex;
+                    width: 100%;
+                    justify-content: space-between;
+                    margin-bottom: 10px;
+                    > div{
+                        p:nth-child(3){
+                            color: #999;
                         }
+                        width: 50%;
+                        text-align: center;
                     }
-                    .number{
-                        font:700 20px DIN Alternate,din_alternate_bold,-apple-system-font,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,Helvetica Neue,PingFang SC,Hiragino Sans GB,Microsoft YaHei UI,Microsoft YaHei,Arial
-                    }
-                    .now_confirm{
-                        color: #e44a3d;
-                    }
-                    .confirm{
-                        color: #a31d13;
-                    }
-                    .input{
-                        color: #ffa352;
-                    }
-                    .dead{
-                        color: #333;
-                    }
-                    .heal{
-                        color: #34aa70;
-                    }
+                }
+                .number{
+                    font:700 20px DIN Alternate,din_alternate_bold,-apple-system-font,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,Helvetica Neue,PingFang SC,Hiragino Sans GB,Microsoft YaHei UI,Microsoft YaHei,Arial
+                }
+                .now_confirm{
+                    color: #e44a3d;
+                }
+                .confirm{
+                    color: #a31d13;
+                }
+                .input{
+                    color: #ffa352;
+                }
+                .dead{
+                    color: #333;
+                }
+                .heal{
+                    color: #34aa70;
                 }
             }
         }
     }
+}
+
+.data-map,
+.data-list {
+  margin-top: 200px;
+  overflow: hidden;
+  h3 {
+    border-left: 8px solid #e10000;
+    padding-left: 1rem;
+  }
+}
+.map-box {
+  display: flex;
+  width: 200%;
+}
+
+#line,
+#line1,
+#line2 {
+  height: 400px;
+  width: 100%;
+}
+
+#map,
+#map2 {
+  height: 350px;
+  width: 50%;
+  animation-fill-mode: forwards;
+}
+.to-left {
+  animation: toLeft 1s;
+}
+
+.to-right {
+  animation: toRight 1s;
+}
+
+#map {
+  margin-right: 2rem;
+  // position: relative;
+}
+.map-btn,
+.line-btn {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  > div {
+    width: 45%;
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #d2d2d2;
+    box-shadow: 0 5px 7px 1px rgb(0 0 0 / 5%);
+    border-radius: 5px;
+    text-align: center;
+    &.active {
+      border-color: #ce4733;
+      background-color: #fef7f7;
+      color: #ce2c1e;
+    }
+  }
+}
+.line-btn {
+  > div {
+    width: 30%;
+    height: 55px;
+    line-height: 25px;
+    padding-top: 5px;
+  }
+}
+.data-list {
+  margin-top: 20px;
+}
 </style>
--- src/pageJS/index.ts
+++ src/pageJS/index.ts
@@ -1,5 +1,10 @@
 import type { IChinaTotal, IData, IGDData } from '../type/index'
 import axios from "axios"
+import * as echarts from 'echarts'
+import chinaJson from "../assets/china.json"
+
+
+type EChartsOption = echarts.EChartsOption
 
 class InitData implements IData{
     areaTree: any[] = []
@@ -23,6 +28,48 @@ class InitData implements IData{
 }
 
 const initDataFunc = (data: InitData)=>{
+    let mapDom: HTMLElement | null = document.getElementById("map")
+    let mapDom2: HTMLElement | null = document.getElementById("map2")
+    let optionMap: EChartsOption = {
+        title: {//标题内容
+            // text: '中国疫情图',
+            subtext: "单击省份可查看病例数",
+        },
+        tooltip: {//提示框组件
+            trigger: "item",//触发类型 数据项图形触发，主要在散点图，饼图等无类目轴的图表中使用。
+            formatter: "现有确诊病例<br/>{b}: {c} ",//提示框浮层内容格式器，支持字符串模板和回调函数两种形式
+            //  模板变量有 { a }, { b }，{ c }，{ d }，{ e }，分别表示系列名，数据名，数据值等。 在 trigger 为 'axis' 的时候，会有多个系列的数据，此时可以通过 { a0 }, { a1 }, { a2 } 这种后面加索引的方式表示系列的索引。 不同图表类型下的 { a }，{ b }，{ c }，{ d } 含义不一样。 其中变量{ a }, { b }, { c }, { d } 在不同图表类型下代表数据含义为：
+
+            // 折线（区域）图、柱状（条形）图、K线图: { a }（系列名称），{ b }（类目值），{ c }（数值）, { d }（无）
+
+            // 散点图（气泡）图: { a }（系列名称），{ b }（数据名称），{ c }（数值数组）, { d }（无）
+
+            // 地图: { a }（系列名称），{ b }（区域名称），{ c }（合并数值）, { d }（无）
+
+            // 饼图、仪表盘、漏斗图: { a }（系列名称），{ b }（数据项名称），{ c }（数值）, { d }（百分比）
+
+        },
+        visualMap: {
+            show: false,
+        },
+    }
+    let series = {
+        type: "map",
+        map: "china",
+        colorBy: "series",
+        zoom: 1,
+        top: 80,
+        label: {
+            show: true,
+            color: "#333",
+            fontSize: 10
+        }
+    }
+    let myMap = echarts.init((mapDom as HTMLElement))
+    let myMap2 = echarts.init((mapDom2 as HTMLElement))
+    echarts.registerMap("china", (chinaJson as any))
+    myMap.showLoading()
+
     axios("/api/ug/api/wuhan/app/data/list-total").then((res)=>{
         if(res.data.code && res.data.code == 10000){
             data.areaTree = res.data.data.areaTree
@@ -30,6 +77,37 @@ const initDataFunc = (data: InitData)=>{
             data.chinaTotal = res.data.data.chinaTotal
             data.china = data.areaTree.find((item: { id: string; }) => item.id === "0").children
             data.gdData = data.china.find((item: { id: string;}) => item.id === "440000")        
+
+            let mapData: any[] = []
+            let mapData2: any[] = []
+            
+            data.china.map( (v: any) => {
+                mapData.push({
+                    name: v.name,
+                    value: v.total.confirm - v.total.dead - v.total.heal || 0
+                })
+                mapData2.push({
+                    name: v.name,
+                    value: v.total.confirm || 0
+                })
+            })
+            myMap.hideLoading()
+            myMap.setOption({
+                ...optionMap,
+                series: {
+                    ...series,
+                    data: mapData,
+
+                }
+            })
+            myMap2.setOption({
+                ...optionMap,
+                series: {
+                    ...series,
+                    data: mapData2,
+
+                }
+            })
         }
     })
 }
